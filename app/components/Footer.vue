3c7dd7570cc2a1ad39248fe4b5906ad24cbed83b
esrafil <elahi.esrafil@okala.com>
2025-08-22 20:08:40 +0330

    add footer component dark mode

--- app/components/Footer.vue
+++ app/components/Footer.vue
@@ -4,15 +4,15 @@
 
 <template>
   <div class="px-[48px] my-10">
-    <h1 class="title-3 text-center">Esrafil Elahi</h1>
+    <h1 class="title-3 text-center dark:text-secondary">Esrafil Elahi</h1>
 
     <div class="flex justify-center items-center gap-4 mt-4">
       <a class="cursor-pointer" href="https://www.linkedin.com/in/esrafil-elahi/" target="_blank">
-        <Icon name="uil:linkedin" size="25" class="cursor-pointer" />
+        <Icon name="uil:linkedin" size="25" class="cursor-pointer dark:text-secondary" />
       </a>
 
       <a class="cursor-pointer" href="https://github.com/EsrafilElahi" target="_blank">
-        <Icon name="uil:github" size="25" class="cursor-pointer" />
+        <Icon name="uil:github" size="25" class="cursor-pointer dark:text-secondary" />
       </a>
     </div>
   </div>
