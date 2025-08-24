266fbbff75c1ad24e4b60e35067d5dcceac6483e
esrafil <elahi.esrafil@okala.com>
2025-08-24 18:24:31 +0330

    remove clg i $api plugin

--- app/plugins/api.ts
+++ app/plugins/api.ts
@@ -18,17 +18,12 @@ export default defineNuxtPlugin(() => {
       }
     },
     onResponse: (error) => {
-      console.log('error :', error)
-
       // console.log("error response :", error);
     },
     onRequestError: (requestError) => {
-      console.log('requestError :', requestError)
-
       // console.log("requestError :", requestError);
     },
     onResponseError: (responseError) => {
-      console.log('responseError :', responseError)
       return Promise.reject(responseError);
 
       // if (responseError.response.status === 401) {
